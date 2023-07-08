922cff37581b752736a1940cc8f06b009d6e76aa
sergey <xt-2000@mail.ru>
2023-07-08 11:19:52 +0300

    fc

---
+++ sem3/Main.java
@@ -0,0 +1,21 @@
+package sem3;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(8);
+        list.add(12);
+
+        list.print();
+        //list.revers();
+        list.revers();
+        list.print();
+
+        
+    }
+}
